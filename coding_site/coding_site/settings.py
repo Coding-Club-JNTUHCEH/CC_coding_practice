5afb04e15e52817ce2035b9a16d3be7910fec6cc
jaya-shankar <bjayashankar2102@gmail.com>
2021-05-19 22:23:45 +0530

    installed django_heroku

--- coding_site/coding_site/settings.py
+++ coding_site/coding_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,3 +134,6 @@ LOGIN_URL='/login'
 
 os.makedirs(STATIC_ROOT, exist_ok=True)
 os.makedirs(STATICFILES_DIRS[0], exist_ok=True)
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
