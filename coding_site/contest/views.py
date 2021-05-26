9ec98e84d5ef79cd753460fcedde6ff213c767b4
sowmya <kayaackerman515@gmail.com>
2021-05-26 13:57:33 +0530

    added contest page

--- coding_site/contest/views.py
+++ coding_site/contest/views.py
@@ -5,35 +5,44 @@ from index.models import Problem
 from users.codeforces_API import fetchAllContests
 from .models import Contest
 from django.http import HttpResponse
+# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
 
 def contest_page(request):
     contests = Contest.objects.filter(name__icontains="Div.").values()
     a = 1
-    for contest in contests:
-        # if a < 10:
-        print(contest)
-        print(contest['name'])
-        print(contest['contestID'])
-        print(contest['type'])
-        # print(contest['problems'])
-        # # contest['problems'] = ['A']
-        # problems = Problem.objects.filter(
-        #     contestID=contest['contestID']).order_by("index")
-        # print(problems)
-        # print(len(problems))
-        # # print(problems)
-        # if problems.exists() and len(problems) > 4:
+    limit = 15
+    if 'contest-all' in request.path:
+        limit = 200
 
-        #     # print(type(problems_list))
-        #     contest['problems'] = list(problems)
-        #     # a += 1
-        # print(contest)
-        # print(', '.join(contest.problems.values_list('name', flat=True)))
-        # a += 1
-    # print(contests)
-    return render(request, 'contest_page.html', {'contests': contests})
+    for contest in contests:
+        if a < limit:
+            print(contest)
+            print(contest['name'])
+            print(contest['contestID'])
+            print(contest['type'])
+            # print(contest['problems'])
+            # contest['problems'] = ['A']
+            problems = Problem.objects.filter(
+                contestID=contest['contestID']).order_by("index")
+            print(problems)
+            print(len(problems))
+            # print(problems)
+            if problems.exists() and len(problems) > 4:
+                # print(type(problems_list))
+                contest['problems'] = list(problems)
+                a += 1
+            # print(contest['problems'])
+            print(contest)
+            # print(', '.join(contest.problems.values_list('name', flat=True)))
+
+        else:
+            break
+
+    print(request.path)
+    path = request.path + '-all'
+    return render(request, 'contest_page.html', {'contests': contests, 'path': path})
 
 
 # https://codeforces.com/api/contest.list
@@ -54,17 +63,12 @@ def loadContests_view(request):
 
         c_db = Contest.create(contest)
         c_db.problems = c_db.add_problems()
-        # print(c_db.problems)
-        # problems = Problem.
-        # c_db.problems.set(problems)
+
         try:
             c_db.save()
             count += 1
-            # p_db.link_tags(problem["tags"])
-            # print(str(a)+".Contest ", c_db, " saved" .format(a))
         except:
             print("Error!!!")
-            # print(str(a)+".Contest ", c_db, " could not add" .format(a))
         a += 1
 
     print('yes!!!!')
