9360f143bb40cee3acd69864095071dbef49c741
Jaya Shankar <bjayashankar2102@gmail.com>
2021-05-30 23:55:57 +0530

    add help page (#14)

--- users/models.py
+++ users/models.py
@@ -1,10 +1,10 @@
-from .codeforces_API import getSolvedProblems
+
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from index.models import Problem
 
-
+from .codeforces_API import getSolvedProblems
 # Create your models here.
 
 class UserProfile(models.Model): 
@@ -30,12 +30,12 @@ class UserProfile(models.Model):
 
     def add_solvedProblems(self):
 
-        solvedProblems = getSolvedProblems(self.codeForces_username)
+        solvedProblems = getSolvedProblems( username = self.codeForces_username )
         for problem in solvedProblems:
             self.add_solvedProblem(problem)
 
     def updated_solvedProblems(self):
-        solvedProblems  = getSolvedProblems(self.codeForces_username)
+        solvedProblems  = getSolvedProblems( username = self.codeForces_username )
         remaing         = len(solvedProblems) - self.sloved_problems.count()
         for p in range(remaing):
             self.add_solvedProblem(solvedProblems[p])
