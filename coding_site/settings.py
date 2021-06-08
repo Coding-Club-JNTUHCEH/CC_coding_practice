54f9a0f09665dbe87494609732b875cc9cf8248f
jaya-shankar <bjayashankar2102@gmail.com>
2021-06-08 19:45:16 +0530

    third deploy

--- coding_site/settings.py
+++ coding_site/settings.py
@@ -138,7 +138,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
