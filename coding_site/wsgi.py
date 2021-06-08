86f2e3667a6ed3788ceb5875ae39a85cb1990170
jaya-shankar <bjayashankar2102@gmail.com>
2021-06-08 19:36:55 +0530

    second delpoy undo change in manage.py

--- coding_site/wsgi.py
+++ coding_site/wsgi.py
@@ -8,12 +8,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 from dotenv import load_dotenv   #for python-dotenv method
 load_dotenv()                    #for python-dotenv method
-
-
 
 
 
@@ -21,4 +17,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coding_site.settings')
 
-application = DjangoWhiteNoise(get_wsgi_application())
+application = get_wsgi_application()
