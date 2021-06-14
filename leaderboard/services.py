2d233dbc6e59b08318fd6118c8c02aa1766846a7
jaya-shankar <bjayashankar2102@gmail.com>
2021-06-14 16:06:11 +0530

    change leaderboard

--- leaderboard/services.py
+++ leaderboard/services.py
@@ -1,9 +1,11 @@
 
 
 def extract_leaderboardData(user,rank):
+    count = user.sloved_problems.all().count()
     return {
         "username" : user.user.username ,
         "rating"   : user.rating,
-        "rank"     : rank
+        "rank"     : rank, 
+        "solved_problems" :count
 
     }
